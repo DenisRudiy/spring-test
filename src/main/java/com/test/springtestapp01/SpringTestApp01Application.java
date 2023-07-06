84716d5445276ffb69c292332d4ea531df1645ff
DenisRudiy <denryd2311@gmail.com>
2023-07-06 15:56:42 +0300

    rework

--- src/main/java/com/test/springtestapp01/SpringTestApp01Application.java
+++ src/main/java/com/test/springtestapp01/SpringTestApp01Application.java
@@ -3,13 +3,15 @@ package com.test.springtestapp01;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringTestApp01Application {
 
-	@GetMapping("/messages")
+	@GetMapping("/message")
 	public String message(){
-		return "Hello World";
+		return "Hello World! Your app is on Azure now! :D";
 	}
 
 	public static void main(String[] args) {
