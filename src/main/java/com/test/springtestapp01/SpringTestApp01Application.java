b1923c0d960bd37d96a570b0def4e94113d399aa
DenisRudiy <denryd2311@gmail.com>
2023-07-06 16:09:27 +0300

    x
    x

--- src/main/java/com/test/springtestapp01/SpringTestApp01Application.java
+++ src/main/java/com/test/springtestapp01/SpringTestApp01Application.java
@@ -6,7 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringTestApp01Application {
+
+	@GetMapping("/message")
+	public String message() {
+		return "Hello World";
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringTestApp01Application.class, args);
 	}
